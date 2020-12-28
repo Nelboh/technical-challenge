6f43ed72fdf48b2ba51eb92ec18f7cdcac94a681
Nelboh <eshobbs7@gmail.com>
2020-12-28 11:26:10 +0000

    Set up basic react app with git repo, as well as basic file structure and bare bones HTML

---
+++ src/components/App/App.js
@@ -0,0 +1,26 @@
+import store from "../../data/store";
+import Settings from "../Settings/Settings";
+
+
+const App = () => (
+  <>
+    <header>
+      <h1 className="title">5-a-side<br />Team Generator</h1>
+    </header>
+
+    <main>
+
+      <Settings />
+
+    </main>
+
+    <footer>
+      <h2 className="footerText">© 2020 Ellie Hobbs</h2>
+    </footer>
+
+  </>
+
+);
+
+
+export default App;
