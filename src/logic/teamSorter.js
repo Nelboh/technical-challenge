a31523636e087fcd3efe253539976654a010e946
Nelboh <eshobbs7@gmail.com>
2020-12-28 16:41:56 +0000

    Made a team splitter inside teamSorter to divide the shuffled teams in two

--- src/logic/teamSorter.js
+++ src/logic/teamSorter.js
@@ -1,30 +1,39 @@
 import store from "../data/store";
 
 // This uses the Fisher-Price algorithm (just kidding, it's Fisher-Yates) to shuffle the names in the array
-const shuffleArray = (inputArray) => {
+const shuffleArray = (array) => {
 
-    for (let i = inputArray.length - 1; i > 0; i--) {
+    for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * i)
-        const temp = inputArray[i]
-        inputArray[i] = inputArray[j]
-        inputArray[j] = temp
+        const temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
     }
 
-    return inputArray;
+    return array;
+
 };
+
+const splitArray = (array) => {
+
+    const half = Math.ceil(array.length / 2);
+
+    const TeamA = array.splice(0, half)
+    const TeamB = array.splice(-half)
+    console.log(TeamA)
+    console.log(TeamB)
+
+}
+
 
 const teamSorter = () => {
     // Make a copy of the original array 
     let originalArray = store.getState().playersArray
     let inputArray = [...originalArray]
 
-    console.log(inputArray)
-    console.log(shuffleArray(inputArray))
-
-    // let shuffleTeam = store.getState().playersArray
-
-
-
+    let shuffledArray = shuffleArray(inputArray)
+    console.log(shuffledArray)
+    splitArray(shuffledArray)
 };
 
 export default teamSorter;
