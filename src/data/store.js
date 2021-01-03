a8d208272aba77299bfff237f1d9789b1e0153f3
Nelboh <eshobbs7@gmail.com>
2021-01-03 12:05:02 +0000

    Removed unused persistence code. All yellow errors now cleared

--- src/data/store.js
+++ src/data/store.js
@@ -1,23 +1,11 @@
-import { createStore, compose } from "redux";
+import { createStore } from "redux";
 import initial from "./initial";
 import reducers from "./reducers";
-// import persistState from "redux-localstorage";
 
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     reducers,
     initial,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    // composeEnhancers(persistState())
 );
-
-// store.subscribe(() => {
-//     let state = store.getState();
-
-// });
-
-// store.dispatch({ type: "ADD_PLAYER" })
-
 
 export default store;
